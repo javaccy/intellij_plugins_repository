e66583942b5ad0fbd91dda37a1576c4a13574533
javaccy <javaccy@gmail.com>
2018-04-25 08:10:08 +0800

    修改

--- src/main/java/com.javaccy.intellij.repository/Main.java
+++ src/main/java/com.javaccy.intellij.repository/Main.java
@@ -4,8 +4,20 @@ import org.eclipse.jetty.server.Server;
 
 public class Main{
 
+    private static int port = 6868;
+
     public static void main(String[] args) throws Exception {
-        Server server = new Server(6868);
+        if (args.length != 0) {
+            try {
+                Integer p = Integer.valueOf(args[0]);
+                if (p > 1024 && p < 65536) {
+                    port = p;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        Server server = new Server(port);
         server.setHandler(new MainHandler());
         server.start();
         server.dumpStdErr();
