85413cac928dd3c8525f1d25dfe55bf33a2f7013
javaccy <javaccy@gmail.com>
2018-04-26 09:07:20 +0800

    修改可用

--- src/main/java/com.javaccy.intellij.repository/MainHandler.java
+++ src/main/java/com.javaccy.intellij.repository/MainHandler.java
@@ -4,12 +4,14 @@ import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.FileUtils;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.net.URLEncoder;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -47,7 +49,23 @@ public class MainHandler extends AbstractHandler {
 
 
     public static String getBasePath(HttpServletRequest request){
-        return "http://127.0.0.1:6868";
+        return "http://plugins.idea.javaccy.giize.com";
+        //return "http://192.168.99.186:6868";
+    }
+
+    public static Double getVersion(String str) {
+        StringBuffer sb = new StringBuffer();
+        boolean flag = true;
+        for (char c : str.toCharArray()) {
+            if ("0123456789".contains(c+"")) {
+                sb.append(c);
+            } else if (c == '.' && flag) {
+                sb.append(c);
+                flag = false;
+            }
+        }
+
+        return Double.valueOf(sb.toString());
     }
 
     /**
@@ -60,8 +78,13 @@ public class MainHandler extends AbstractHandler {
         BufferedOutputStream out = null;
         try {
             String s = getUploadDir() + File.separator + request.getRequestURI().substring(downloadPath.length(), request.getRequestURI().length());
-            in = new BufferedInputStream(new FileInputStream(s));
+            File file = new File(s);
+            FileInputStream f = new FileInputStream(file);
+            in = new BufferedInputStream(f);
             out = new BufferedOutputStream(response.getOutputStream());
+            String filename=URLEncoder.encode(file.getName(),"utf-8");
+            response.setHeader("Content-Disposition","attachment; filename="+filename+"");
+            response.setCharacterEncoding("utf-8");
             byte[] bytes = new byte[1024];
             int len = -1;
             while ((len = in.read(bytes))!=-1) {
@@ -88,22 +111,43 @@ public class MainHandler extends AbstractHandler {
     }
 
     public static void updatePlugins(HttpServletRequest request, HttpServletResponse response){
+        //IC-181.4445.78
+        String build = request.getParameter("build");
+        System.out.println("build:"+build);
+        Double clientVer = 0D;
+        int clientBigVer = 0;
+        if (build != null) {
+            clientVer = getVersion(build);
+            clientVer.intValue();
+        }
+        Double version;
+        int bigVersion;
+        String fileName;
         PrintWriter out = null;
         try {
             response.setHeader("Content-Type","application/xml");
             out = response.getWriter();
             File dir = getUploadDir();
             out.print("<plugins>");
-            String version = "";
             for (File username : dir.listFiles()) {
                 if (username.isDirectory()) {
                     for (File id : username.listFiles()) {
                         if (id.isDirectory()) {
                             for (File file : id.listFiles()) {
-                                version = file.getName().substring(file.getName().indexOf(".")+2,file.getName().lastIndexOf("."));
-                                if (file.getName().endsWith(".zip") || file.getName().endsWith(".jar")) {
-                                    out.print("<plugin id=\"" + id.getName() + "\" url=\"" + getBasePath(request) + downloadPath + username.getName() + File.separator + id.getName() + File.separator + file.getName() + "\" version=\""+version+"\"/>");
+
+                                fileName = file.getName();
+                                if (file.isDirectory()||fileName.startsWith(".")) {
+                                    continue;
                                 }
+                                version = getVersion(fileName);
+                                bigVersion = version.intValue();
+                                //判断大版本是否相同，例如：181.1124.23 = 181.32445.999
+                                if (bigVersion == clientBigVer || clientBigVer == 0) {//clientBigVer = 0时全部返回方便测试
+                                    if (file.getName().endsWith(".zip") || file.getName().endsWith(".jar")) {
+                                        out.print("<plugin id=\"" + id.getName() + "\" url=\"" + getBasePath(request) + downloadPath + username.getName() + File.separator + id.getName() + File.separator + file.getName() + "\" version=\""+version+"\"/>");
+                                    }
+                                }
+
                             }
                         }
                     }
@@ -183,9 +227,12 @@ public class MainHandler extends AbstractHandler {
                         fields.put(fileItem.getFieldName(), fileItem.getString());
                     }
                 }
+                File file;
                 for (FileItem fileItem : fileItems) {
                     if(!fileItem.isFormField()) {
-                        fileItem.write(new File(getUploadDir(fields.get("userName"),fields.get("xmlId")) + File.separator +  fileItem.getName()));
+                        file = new File(getUploadDir(fields.get("userName"),fields.get("xmlId")) + File.separator +  fileItem.getName());
+                        FileUtils.copyInputStreamToFile(fileItem.getInputStream(), file);
+
                     }
                 }
                 out = response.getWriter();
