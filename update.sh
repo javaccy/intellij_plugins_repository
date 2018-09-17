1fdd1eeafdaf44f6be29ca61e101216739f2fe65
javaccy <javaccy@gmail.com>
2018-09-18 01:09:20 +0800

    添加了一个bash上传脚本

---
+++ update.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+proxychains4 scp -rv -P 3333 /Users/javaccy/IdeaPluginProjects/intellij_plugins_repository/build/distributions/intellij_plugins_repository-1.0-SNAPSHOT/lib/intellij_plugins_repository-1.0-SNAPSHOT.jar xxx@xxx:apps/intellij/lib/.
